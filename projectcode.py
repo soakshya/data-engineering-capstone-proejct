769d17471ee850b60275da5189b9a1e469065210
soakshya <soakshya1127@gmail.com>
2023-09-01 12:05:41 -0700

    test

---
+++ projectcode.py
@@ -0,0 +1,10 @@
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAZJ5SE5IUMHMNXYJM")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+df_hospital = spark.read.option("header","true").csv("s3://buck1234/Capst/hospital.csv")
+df_hospital.show()
+
+
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAZJ5SE5IUMHMNXYJM")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+df_Patientsrecord = spark.read.option("header","true").csv("s3://buck1234/Capst/Patient_records.csv")
+df_Patientsrecord.show()
